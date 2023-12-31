45de080ac44a244388284a5df301f737896121be
mohamed-gaaya <contact.mohamed.gaaya@gmail.com>
2023-12-31 17:13:42 +0100

    a

---
+++ src/main/java/org/example/service/IBookingService.java
@@ -0,0 +1,19 @@
+package org.example.service;
+
+import org.example.model.BookedRoom;
+
+import java.util.List;
+
+public interface IBookingService {
+    void cancelBooking(Long bookingId);
+
+    List<BookedRoom> getAllBookingsByRoomId(Long roomId);
+
+    String saveBooking(Long roomId, BookedRoom bookingRequest);
+
+    BookedRoom findByBookingConfirmationCode(String confirmationCode);
+
+    List<BookedRoom> getAllBookings();
+
+    List<BookedRoom> getBookingsByUserEmail(String email);
+}
---
+++ src/main/java/org/example/sovivaResortHotelApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class sovivaResortHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(sovivaResortHotelApplication.class, args);
+    }
+}
